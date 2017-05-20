201f0b2a7b971e4921edf8eae9fa45ea22b36c1a
Ricardo Azevedo <Ricardo_Azevedo@iscte-iul.pt>
2017-05-20 20:20:53 +0100

    US ready for release

--- src/main/java/mt/persistence/XMLSaver.java
+++ src/main/java/mt/persistence/XMLSaver.java
@@ -1,34 +1,27 @@
 package mt.persistence;
 
 
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
-import java.nio.file.Files;
-
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.transform.OutputKeys;
-import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerConfigurationException;
-import javax.xml.transform.TransformerException;
-import javax.xml.transform.TransformerFactory;
-import javax.xml.transform.TransformerFactoryConfigurationError;
-import javax.xml.transform.dom.DOMSource;
-import javax.xml.transform.stream.StreamResult;
-
+import mt.Order;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.xml.sax.SAXException;
 
-import mt.Order;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.*;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
 
 public class XMLSaver {
 
 	/*
-	 * FOR TEST
+     * FOR TEST
 	 * 
 
 	public static void main(String[] args) {
@@ -48,58 +41,56 @@ public class XMLSaver {
 	}
 	
 	 */
-	
 
-	
-	
-	private File file;
-	private Document document;
-	private Transformer transformer;
 
-	public XMLSaver(File file) {
-		this.file = file;
-	}
+    private File file;
+    private Document document;
+    private Transformer transformer;
 
-	public XMLSaver init() throws TransformerConfigurationException, TransformerFactoryConfigurationError, SAXException,
-			IOException, ParserConfigurationException {
-		try (InputStream input = getClass().getClassLoader().getResourceAsStream(file.getName())) {
-			if (!file.exists())
-				Files.copy(input, file.toPath());
-		} catch (IOException ignore) {
-		}
-		DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
-		DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
-		document = dBuilder.parse(this.file);
+    public XMLSaver(File file) {
+        this.file = file;
+    }
 
-		transformer = TransformerFactory.newInstance().newTransformer();
-		transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+    public XMLSaver init() throws TransformerConfigurationException, TransformerFactoryConfigurationError, SAXException,
+            IOException, ParserConfigurationException {
+        try (InputStream input = getClass().getClassLoader().getResourceAsStream(file.getName())) {
+            if (!file.exists())
+                Files.copy(input, file.toPath());
+        } catch (IOException ignore) {
+        }
+        DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+        document = dBuilder.parse(this.file);
 
-		return this;
-	}
+        transformer = TransformerFactory.newInstance().newTransformer();
+        transformer.setOutputProperty(OutputKeys.INDENT, "yes");
 
-	private Element toXML(Order order) {
+        return this;
+    }
 
-		Element orderXML = document.createElement("Order");
-		orderXML.setAttribute("Id", String.valueOf(order.getServerOrderID()));
-		orderXML.setAttribute("Type", order.isBuyOrder() ? "buy" : "sel");
-		orderXML.setAttribute("Stock", order.getStock());
-		orderXML.setAttribute("Units", String.valueOf(order.getNumberOfUnits()));
-		orderXML.setAttribute("Price", String.valueOf(order.getPricePerUnit()));
-		
+    private Element toXML(Order order) {
+
+        Element orderXML = document.createElement("Order");
+        orderXML.setAttribute("Id", String.valueOf(order.getServerOrderID()));
+        orderXML.setAttribute("Type", order.isBuyOrder() ? "buy" : "sel");
+        orderXML.setAttribute("Stock", order.getStock());
+        orderXML.setAttribute("Units", String.valueOf(order.getNumberOfUnits()));
+        orderXML.setAttribute("Price", String.valueOf(order.getPricePerUnit()));
+
 //		Element costumer = document.createElement("costumer");
 //		costumer.appendChild(document.createTextNode(order.getNickname()));
 //		orderXML.appendChild(costumer);
-		
-		return orderXML;
-	}
 
-	public void save(Order order) throws TransformerException {
-		Node node = document.getDocumentElement();
-		node.appendChild(toXML(order));
+        return orderXML;
+    }
 
-		StreamResult result = new StreamResult(file);
-		DOMSource source = new DOMSource(document);
-		transformer.transform(source, result);
-	}
+    public void save(Order order) throws TransformerException {
+        Node node = document.getDocumentElement();
+        node.appendChild(toXML(order));
+
+        StreamResult result = new StreamResult(file);
+        DOMSource source = new DOMSource(document);
+        transformer.transform(source, result);
+    }
 
 }
